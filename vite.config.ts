0438576ada346bfa063aa752f2c475a6d5ac10f0
Sharran Sreekant <sharransreekant@Sharrans-MacBook-Pro.local>
2026-03-16 22:13:18 +1100

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Esha-v2/', // GitHub Pages repo name — change to /Esha/ when you swap repos
+  base: '/Esha/', // GitHub Pages repo name — change to /Esha/ when you swap repos
 })
